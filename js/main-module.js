6a947099e9245865014c31cb02fca293d3ede84d
Claudio <claudiolombardi00@gmail.com>
2021-05-13 10:48:06 +0200

    aggiunta funzionalità alla barra di ricerca e code review

--- js/main-module.js
+++ js/main-module.js
@@ -1,45 +1,22 @@
-export const searchFilm = () => {
+export const searchFilm = (input, container) => {
+    const movie = document.getElementById(input).value;
+    clearContainer(container);
+    addFilm(movie);
+}
 
-    const movie = document.getElementById("myInput").value;
+const clearContainer = (container) => {
+    document.getElementById(container).innerHTML = '';
+}
 
+export const addFilm = (movie) => {
     const promise = fetch('https://www.omdbapi.com/?&apikey=d99eb7c1&s=' + movie);
-
     promise
-
-        .then(res => { if (res) return res.json(); })
-
-        .then(resjson => {
-            for (let i = 0; i < resjson.Search.length; i++) {
-                console.log(resjson.Search[i]);
-            }
-        })
-
-        .catch(err => { console.error(err); })
-
+        .then(res => { if (res) return res.json(); }) //recupero il contenuto json
+        .then(resjson => { resjson.Search.forEach(createFilm); })
+        .catch(err => { console.error(err); }) //solo in caso di errore stampo nella console
 }
 
-
-export const film = () => {
-    const promise = fetch(
-        'https://www.omdbapi.com/?&apikey=d99eb7c1&s=Fast+Furious'
-    );
-    promise
-    //recuper il contenuto json
-        .then(res => {
-            if (res) return res.json();
-        })
-        .then(resjson => {
-          //  console.log(resjson)
-            resjson.Search.forEach(stampa);
-        })
-        //solo in caso di errore stampo nella console
-        .catch(err => {
-            console.error(err);
-        })
-}
-
-const stampa = (item) => {
-
+const createFilm = (item) => {
     //creo tutti i div//
     const MAINdiv = document.createElement('div');
     const divCard = document.createElement('div');
@@ -48,30 +25,34 @@ const stampa = (item) => {
     const card_text_anno = document.createElement("p");
     const card_text_genere = document.createElement("p");
     const container = document.getElementById('mainContainer');
+    makeCard(MAINdiv, divCard, poster, card_title, card_text_anno, card_text_genere);
+    addContent(item, poster, card_title, card_text_anno, card_text_genere);
+    //inserisco le parti della card nel divcard e la div card nel MAINdiv 
+    divCard.innerHTML += poster.outerHTML + card_title.outerHTML + card_text_anno.outerHTML + card_text_genere.outerHTML
+    MAINdiv.appendChild(divCard);
+    container.appendChild(MAINdiv);
+}
+
+const makeCard = (div, card, poster, title, year, genre) => {
     //assegno attributi ai div creati
-    MAINdiv.setAttribute("class", "col");
-    divCard.setAttribute("class", "card");
+    div.setAttribute("class", "col");
+    card.setAttribute("class", "card");
     poster.setAttribute("class", "card-img-top")
+    //assegno attributi ai div creati
+    title.setAttribute("class", "card-title");
+    year.setAttribute("class", "card-text");
+    genre.setAttribute("class", "card-text");
+}
+
+const addContent = (item, poster, title, year, genre) => {
     //controllo se il poster e' presente, altrimenti inserisco un immagine 
     if (item.Poster == "N/A") {
         poster.setAttribute("src", ("img/not-found.jpg"));
     } else {
         poster.setAttribute("src", item.Poster);
     }
-    //assegno attributi ai div creati
-    card_title.setAttribute("class", "card-title");
-    card_text_anno.setAttribute("class", "card-text");
-    card_text_genere.setAttribute("class", "card-text");
-    //inserisco le stringhe nell html
-    card_title.innerHTML = "Titolo: " + item.Title;
-    card_text_anno.innerHTML = "Anno: " + item.Year;
-    card_text_genere.innerHTML = "Genere: " + item.Type;
-    //inserisco le parti della card nel divcard e la div card nel MAINdiv 
-    MAINdiv.appendChild(divCard);
-    divCard.appendChild(poster);
-    divCard.appendChild(card_title);
-    divCard.appendChild(card_text_anno);
-    divCard.appendChild(card_text_genere);
-    container.appendChild(MAINdiv);
+    //inserisco le stringhe nell'html
+    title.innerHTML = "Titolo: " + item.Title;
+    year.innerHTML = "Anno: " + item.Year;
+    genre.innerHTML = "Genere: " + item.Type;
 }
- 
